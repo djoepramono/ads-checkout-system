f2853da899b0be01bb0bb9e2c6c6444958e7ae6c
Djoe Pramono <djoepramono@gmail.com>
2019-10-26 01:55:31 +1100

    add shared data type

---
+++ src/models.ts
@@ -0,0 +1,38 @@
+export enum Ad {
+  Classic = 'CLASSIC',
+  StandOut = 'STANDOUT',
+  Premium = 'PREMIUM'
+}
+
+export interface Advertisement {
+  ad: Ad;
+  description: string;
+  retailPrice: number;
+};
+
+export interface CartItem {
+  ad: Ad;
+  count: number;
+  retailPrice: number;
+}
+
+
+// move to Deal?
+export interface DiscountedAdvertisement {
+  discountedPrice: number;
+};
+
+export interface GetXForY {
+  getCount: number;
+  forCount: number;
+};
+
+export type Deal = DiscountedAdvertisement | GetXForY
+
+export const isDiscountedAdvertisement = (deal: Deal): deal is DiscountedAdvertisement => {
+  return (deal as DiscountedAdvertisement).discountedPrice !== undefined
+};
+
+export const isGetXForY = (deal: Deal): deal is GetXForY => {
+  return (deal as GetXForY).getCount !== undefined;
+}
